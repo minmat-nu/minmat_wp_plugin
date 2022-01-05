7759b6514d2510f74832e2f7d064e56d84e3a4eb
KaptenIT <ojsan96@gmail.com>
2022-01-05 02:10:47 +0100

    added submenu page
    added submenu page but with default content

--- admin_panel.php
+++ admin_panel.php
@@ -3,10 +3,12 @@
 
 function minmat_plugin_menu() {
        add_menu_page( 'MinMat', 'MinMat.nu', 'manage_options', 'minmat-options', 'minmat_options', 3);
+       add_submenu_page( 'minmat-options' , 'Editera Databas', 'Editera Databas', 'manage_options', 'submenu', 'minmat_edit_database');
     }
 
 
     function minmat_options() {
+        echo "<h3> MinMat.nu</h3>";
               if ( !current_user_can( 'manage_options' ) )  {
                         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
                 }
@@ -14,4 +16,15 @@ function minmat_plugin_menu() {
               echo '<p>Here is where the form would go if I actually had options.</p>';
               echo '</div>';
         }
+
+    function minmat_edit_database() {
+        echo "<h3> Editera Databas</h3>";
+        if ( !current_user_can( 'manage_options' ) )  {
+                  wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+          }
+        echo '<div class="wrap">';
+        echo '<p>Here is where the form would go if I actually had options.</p>';
+        echo '</div>';
+    }
+
  ?>
